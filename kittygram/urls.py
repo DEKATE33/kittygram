9a529ea75c833430a5d83f50b9bfe762d6595d16
Artem Dirizhenko <dekate33@outlook.com>
2023-12-07 21:18:15 +0300

    Views & Generics

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from cats.views import cat_list
+from cats.views import cat_list, APICat, CatList, CatDetail
+
 
 urlpatterns = [
-   path('cats/', cat_list),
+   # path('cats/', cat_list),
+   # path('cats/', APICat.as_view()),
+   path('cats/', CatList.as_view()),
+   path('cats/<int:pk>/', CatDetail.as_view()),
 ]
 
 
